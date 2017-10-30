e3df3667f481426ff0ed8eb2723a0986086dedfd
jyeatman <jyeatman@.uw.edu>
2017-10-30 13:42:51 -0700

    fixed simulation to use prc rather than sd

--- simulateAgeEffect.m
+++ simulateAgeEffect.m
@@ -14,20 +14,36 @@ noiseSD = .4
 mp = 1;
 
 % Simulation parameters
-params = [7 1 1 0; 7 2 1 0; 7 3 1 0; 7 4 1 0; 7 5 1 0; 7 6 1 0]
-nrep = 100;
+params = horzcat(repmat(min(ages), [11, 1]), [1:.5:6]', repmat(mp, [11, 1]), zeros(11,1));
+nrep = 1000; % number of iterations of simulated data
+
+% Adjust the mean plasticity to instead reflect the max plasticity. This
+% adjustment determines the peak of the gaussian.
+% TODO
+
+%% Run simulation
 
 for ss = 1:size(params,1)
+    fprintf('\nRunning %d iterations of simulation %d\n', nrep, ss)
+    % Generate a simulation of the defined effect + noise
     simdata = repmat(evalgaussian1d(params(ss,:),ages),nrep, 1);
-    simnoise = randn(size(simdata)).*noiseSD;
+    simnoise = randn(size(simdata)) .* noiseSD;
     simdata = simdata + simnoise;
+    
+    % Fit the sensitive period model to each instance of the data
     for ii = 1:nrep
         simparams(ii,:,ss) = fitgaussian1d_sd(ages, simdata(ii,:), params(ss,:));
     end
 end
 
-%% Plot
-std_simparams = squeeze(std(simparams));
+%% Plot results
+
+% Calculate 68%CI for params
+prc = prctile(simparams, [16 84],1);
+% Extract just the SD param
+prc = squeeze(prc(:,2,:));
+
+% Plot simulated sensitive period
 figure;
 subplot(1,2,1); hold
 x0 = 7:.1:20;
@@ -39,27 +55,18 @@ patch([min(ages) max(ages) max(ages) min(ages) min(ages)],[0 0 1 1 0],[.5 .5 .5]
     'edgealpha',.3,'facealpha',.3)
 axis('tight')
 xlabel('Age'); ylabel('Plastiity');
+
+% Plot error on estimated parameters
 subplot(1,2,2); hold
 for ii = 1:size(params,1)-1
-    plot([params(ii,2) params(ii+1,2)],[std_simparams(2,ii) std_simparams(2,ii+1)],'-', 'color', c(ii,:));
-    plot(params(ii,2),std_simparams(2,ii),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
+    plot([params(ii,2) params(ii+1,2)],[diff(prc(:,ii)) diff(prc(:,ii+1))],'-', 'color', c(ii,:));
+    plot(params(ii,2), diff(prc(:,ii)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
 end
-plot(params(end,2),std_simparams(2,end),'o', 'color', c(end,:), 'markerfacecolor', c(end,:));
+plot(params(end,2), diff(prc(:,end)),'o', 'color', c(end,:), 'markerfacecolor', c(end,:));
 
 axis([0 6 0 24]);
 xlabel('Sensitive period width'); ylabel('Estimation error');
-% 
-% subplot(2,2,3); hold
-% for ii = 1:size(params,1)
-%     plot(x0, evalPoissonCurve(paramsPois(ii,:),x0),'-','color',c(ii,:));
-% end
-% subplot(2,2,2); hold
-% for ii = 1:size(params,1)-1
-%     plot([params(ii,2) params(ii+1,2)],[std_simparams(2,ii) std_simparams(2,ii+1)],'-', 'color', c(ii,:));
-%     plot(params(ii,2),std_simparams(2,ii),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
-% end
 
-% parameter distributsion
 
 return 
 
