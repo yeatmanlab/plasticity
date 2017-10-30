5caae81b5f6e468cf8621423b551648d283bfd81
jyeatman <jyeatman@.uw.edu>
2017-10-30 15:25:03 -0700

    filled in real data for IFOF

--- simulateAgeEffect.m
+++ simulateAgeEffect.m
@@ -7,14 +7,14 @@ ages = 7:.24:13
 % controls are not showing change over the 8 weeks, then we can calculate
 % the standard deviation of change scores in the control subjects as an
 % estimate of noise.
-noiseSD = .4
+noiseSD = 0.0084;
 
 % Mean plasticity over the age range. This is defined based on the average
 % growth in the intervention subjects
-mp = 1;
+mp = .0054;
 
 % Simulation parameters
-params = horzcat(repmat(min(ages), [11, 1]), [1:.5:6]', repmat(mp, [11, 1]), zeros(11,1));
+params = horzcat(repmat(min(ages), [12, 1]), [.5:.5:6]', repmat(mp, [12, 1]), zeros(12,1));
 nrep = 1000; % number of iterations of simulated data
 
 % Adjust the mean plasticity to instead reflect the max plasticity. This
@@ -51,20 +51,27 @@ c = parula(size(params,1));
 for ii = 1:size(params,1)
     plot(x0, evalgaussian1d(params(ii,:),x0),'-','color',c(ii,:));
 end
-patch([min(ages) max(ages) max(ages) min(ages) min(ages)],[0 0 1 1 0],[.5 .5 .5],...
+patch([min(ages) max(ages) max(ages) min(ages) min(ages)],[0 0 mp mp 0].*1.05,[.5 .5 .5],...
     'edgealpha',.3,'facealpha',.3)
 axis('tight')
 xlabel('Age'); ylabel('Plastiity');
 
+% Add the origin to the plot. We can assume there would be zero error for
+% and SD=0;
+params = vertcat([min(ages), 0, mp, 0], params);
+prc = horzcat([0; 0], prc);
 % Plot error on estimated parameters
 subplot(1,2,2); hold
 for ii = 1:size(params,1)-1
     plot([params(ii,2) params(ii+1,2)],[diff(prc(:,ii)) diff(prc(:,ii+1))],'-', 'color', c(ii,:));
-    plot(params(ii,2), diff(prc(:,ii)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
+    plot(params(ii+1,2), diff(prc(:,ii+1)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
 end
-plot(params(end,2), diff(prc(:,end)),'o', 'color', c(end,:), 'markerfacecolor', c(end,:));
+%plot(params(end,2), diff(prc(:,end)),'o', 'color', c(end,:), 'markerfacecolor', c(end,:));
 
-axis([0 6 0 24]);
+% Add the origin to the plot. This is
+axis tight
+set(gca, 'xtick',0:2:6);
+grid('on')
 xlabel('Sensitive period width'); ylabel('Estimation error');
 
 
