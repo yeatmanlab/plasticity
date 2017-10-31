290762bbbaa44be9111e086adef76b425d823ddb
jyeatman <jyeatman@stanford.edu>
2017-10-30 21:38:52 -0700

    added simulation of correlation and CI

--- simulateAgeEffect.m
+++ simulateAgeEffect.m
@@ -14,7 +14,7 @@ noiseSD = 0.0084;
 mp = .0054;
 
 % Simulation parameters
-params = horzcat(repmat(min(ages), [12, 1]), [.5:.5:6]', repmat(mp, [12, 1]), zeros(12,1));
+params = horzcat(repmat(min(ages), [26, 1]), [1:.2:6]', repmat(mp, [26, 1]), zeros(26,1));
 nrep = 1000; % number of iterations of simulated data
 
 % Adjust the mean plasticity to instead reflect the max plasticity. This
@@ -23,13 +23,14 @@ nrep = 1000; % number of iterations of simulated data
 % ages defined above. If we are hypothesizing that this mean came from data
 % collected over a period with gaussian falloff, then this gaussian would
 % have peaked much above the mean (for the younger subjects).
+params_scaled = params;
 for ss = 1:size(params,1)
     % Calculate how much higher the peak is than the mean over this age
     % range, and then scale gaussian peak parameter appropriately
-    params(ss,3) =  params(ss,3).*mp/mean(evalgaussian1d(params(ss,:),ages));
+    params_scaled(ss,3) =  params(ss,3).*mp/mean(evalgaussian1d(params(ss,:),ages));
     % Test to make sure the new mean is equal to what it should be within
     % numerical precision
-    assert(mean(evalgaussian1d(params(ss,:),ages)) - mp < 10^-16);
+    assert(mean(evalgaussian1d(params_scaled(ss,:),ages)) - mp < 10^-16);
 end
 
 %% Run simulation
@@ -40,27 +41,40 @@ for ss = 1:size(params,1)
     simdata = repmat(evalgaussian1d(params(ss,:),ages),nrep, 1);
     simnoise = randn(size(simdata)) .* noiseSD;
     simdata = simdata + simnoise;
+    simdata_scaled = repmat(evalgaussian1d(params_scaled(ss,:),ages),nrep, 1);
+    % Generate independent noise
+    simnoise = randn(size(simdata_scaled)) .* noiseSD;
+    simdata_scaled = simdata_scaled + simnoise;
     
     % Fit the sensitive period model to each instance of the data
     for ii = 1:nrep
         simparams(ii,:,ss) = fitgaussian1d_sd(ages, simdata(ii,:), params(ss,:));
+        simparams_scaled(ii,:,ss) = fitgaussian1d_sd(ages, simdata_scaled(ii,:), params_scaled(ss,:));
+        % Also compute correlation with age
+        r(ii,ss) = corr(ages', simdata(ii,:)');
+        r_scaled(ii,ss) = corr(ages', simdata_scaled(ii,:)');
     end
 end
 
 %% Plot results
 
+plotparams = [1 2 3 4 5 6]; % numbers to show on plots
 % Calculate 68%CI for params
 prc = prctile(simparams, [16 84],1);
+prc_scaled = prctile(simparams_scaled, [16 84],1);
 % Extract just the SD param
 prc = squeeze(prc(:,2,:));
+prc_scaled = squeeze(prc_scaled(:,2,:));
 
 % Plot simulated sensitive period
 figure;
-subplot(1,2,1); hold
+subplot(1,4,1); hold
 x0 = 7:.1:20;
 c = parula(size(params,1));
 for ii = 1:size(params,1)
-    plot(x0, evalgaussian1d(params(ii,:),x0),'-','color',c(ii,:));
+    if any(params(ii,2) == plotparams)
+        plot(x0, evalgaussian1d(params(ii,:),x0),'-','color',c(ii,:));
+    end
 end
 patch([min(ages) max(ages) max(ages) min(ages) min(ages)],[0 0 max(params(:,3)) max(params(:,3)) 0].*1.05,[.5 .5 .5],...
     'edgealpha',.3,'facealpha',.3);
@@ -68,25 +82,56 @@ plot([min(ages) max(ages)], [mp mp], '--k');
 axis('tight')
 xlabel('Age'); ylabel('Plastiity');
 
-% Add the origin to the plot. We can assume there would be zero error for
-% and SD=0;
-params = vertcat([min(ages), 0, mp, 0], params);
-prc = horzcat([0; 0], prc);
-
 % Plot error on estimated parameters
-subplot(1,2,2); hold
-for ii = 1:size(params,1)-1
-    plot([params(ii,2) params(ii+1,2)],[diff(prc(:,ii)) diff(prc(:,ii+1))],'-', 'color', c(ii,:));
-    plot(params(ii+1,2), diff(prc(:,ii+1)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
-end
+subplot(1,4,2); hold
+ii = 1
+plot(params(ii,2), diff(prc(:,ii)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
+plot(params_scaled(ii,2), diff(prc_scaled(:,ii)),'o', 'color', [0 0 0], 'markerfacecolor', c(ii,:));
 
-% Add the origin to the plot. This is
+for ii = 2:size(params,1)
+    % Plot unscaled as solid line
+    plot([params(ii-1,2) params(ii,2)],[diff(prc(:,ii-1)) diff(prc(:,ii))],'-', 'color', c(ii,:));
+    plot([params_scaled(ii-1,2) params_scaled(ii,2)],[diff(prc_scaled(:,ii-1)) diff(prc_scaled(:,ii))],'-', 'color', c(ii,:));
+    if any(params(ii,2) == plotparams)
+        plot(params(ii,2), diff(prc(:,ii)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
+        plot(params_scaled(ii,2), diff(prc_scaled(:,ii)),'o', 'color', [0 0 0], 'markerfacecolor', c(ii,:));
+    end
+end
 axis tight
 set(gca, 'xtick',0:2:6);
 grid('on')
 xlabel('Sensitive period width'); ylabel('Estimation error');
 
+% Plot 68%CI on estimated parameters
+subplot(1,4,3); hold
 
-return
+for ii = 1:size(params,1)
+    plot([params_scaled(ii,2) params_scaled(ii,2)],prc_scaled(:,ii),'-o', 'color', c(ii,:));
+end
+axis tight
+set(gca, 'xtick',0:2:6);
+grid('on')
+xlabel('Sensitive period width'); ylabel('68% CI on parameter estimate');
+
+% Probability of detecting a negative correlation
+subplot(1,4,4); hold
+rp = sum(r<0)./size(r,1);
+rp_scaled = sum(r_scaled<0)./size(r_scaled,1);
+
+ii = 1;
+plot(params(ii,2), rp(:,1),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
+plot(params(ii,2), rp_scaled(:,ii),'o', 'color', [0 0 0], 'markerfacecolor', c(ii,:));
+
+for ii = 2:size(params,1)
+    % Plot unscaled as solid line
+    plot([params(ii-1,2) params(ii,2)],[rp(:,ii-1) rp(:,ii)],'-', 'color', c(ii,:));
+    plot([params(ii-1,2) params(ii,2)],[rp_scaled(:,ii-1) rp_scaled(:,ii)],'-', 'color', c(ii,:));
+    if any(params(ii,2) == plotparams)
+        plot(params(ii,2), rp(:,ii),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
+        plot(params(ii,2), rp_scaled(:,ii),'o', 'color', [0 0 0], 'markerfacecolor', c(ii,:));
+    end
+end
+xlabel('Sensitive period width'); ylabel('Power');
+grid('on')
 
 
