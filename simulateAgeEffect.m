9a60bbb2394670161c603a86916b24db582287f4
jyeatman <jyeatman@.uw.edu>
2017-10-31 08:50:21 -0700

    added plot of CI and funcitoned scalePeak

--- simulateAgeEffect.m
+++ simulateAgeEffect.m
@@ -23,15 +23,11 @@ nrep = 1000; % number of iterations of simulated data
 % ages defined above. If we are hypothesizing that this mean came from data
 % collected over a period with gaussian falloff, then this gaussian would
 % have peaked much above the mean (for the younger subjects).
-params_scaled = params;
-for ss = 1:size(params,1)
-    % Calculate how much higher the peak is than the mean over this age
-    % range, and then scale gaussian peak parameter appropriately
-    params_scaled(ss,3) =  params(ss,3).*mp/mean(evalgaussian1d(params(ss,:),ages));
-    % Test to make sure the new mean is equal to what it should be within
-    % numerical precision
-    assert(mean(evalgaussian1d(params_scaled(ss,:),ages)) - mp < 10^-16);
+scaleGaussian = true;
+if scaleGaussian
+    params = scalePeak(params, mp, ages);
 end
+
 
 %% Run simulation
 
@@ -73,7 +69,7 @@ x0 = 7:.1:20;
 c = parula(size(params,1));
 for ii = 1:size(params,1)
     if any(params(ii,2) == plotparams)
-        plot(x0, evalgaussian1d(params(ii,:),x0),'-','color',c(ii,:));
+        plot(x0, evalgaussian1d(params(ii,:),x0),'-','color',c(ii,:),'linewidth',3);
     end
 end
 patch([min(ages) max(ages) max(ages) min(ages) min(ages)],[0 0 max(params(:,3)) max(params(:,3)) 0].*1.05,[.5 .5 .5],...
@@ -90,8 +86,8 @@ plot(params_scaled(ii,2), diff(prc_scaled(:,ii)),'o', 'color', [0 0 0], 'markerf
 
 for ii = 2:size(params,1)
     % Plot unscaled as solid line
-    plot([params(ii-1,2) params(ii,2)],[diff(prc(:,ii-1)) diff(prc(:,ii))],'-', 'color', c(ii,:));
-    plot([params_scaled(ii-1,2) params_scaled(ii,2)],[diff(prc_scaled(:,ii-1)) diff(prc_scaled(:,ii))],'-', 'color', c(ii,:));
+    plot([params(ii-1,2) params(ii,2)],[diff(prc(:,ii-1)) diff(prc(:,ii))],'-', 'color', c(ii,:),'linewidth',3);
+    plot([params_scaled(ii-1,2) params_scaled(ii,2)],[diff(prc_scaled(:,ii-1)) diff(prc_scaled(:,ii))],'-', 'color', c(ii,:),'linewidth',3);
     if any(params(ii,2) == plotparams)
         plot(params(ii,2), diff(prc(:,ii)),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
         plot(params_scaled(ii,2), diff(prc_scaled(:,ii)),'o', 'color', [0 0 0], 'markerfacecolor', c(ii,:));
@@ -105,8 +101,14 @@ xlabel('Sensitive period width'); ylabel('Estimation error');
 % Plot 68%CI on estimated parameters
 subplot(1,4,3); hold
 
-for ii = 1:size(params,1)
-    plot([params_scaled(ii,2) params_scaled(ii,2)],prc_scaled(:,ii),'-o', 'color', c(ii,:));
+for ii = 2:size(params,1)
+    plot([params_scaled(ii-1,2) params_scaled(ii,2)],[prc_scaled(1,ii-1) prc_scaled(1,ii)],'-', 'color', c(ii,:),'linewidth',3);
+    plot([params_scaled(ii-1,2) params_scaled(ii,2)],[prc_scaled(2,ii-1) prc_scaled(2,ii)],'-', 'color', c(ii,:),'linewidth',3);
+    if any(params(ii,2) == plotparams)
+        patch([params_scaled(ii,2)-.1, params_scaled(ii,2)+.1, params_scaled(ii,2)+.1,params_scaled(ii,2)-.1,params_scaled(ii,2)-.1]...
+            ,[prc_scaled(1,ii),prc_scaled(1,ii),prc_scaled(2,ii),prc_scaled(2,ii),prc_scaled(1,ii)]...
+            ,c(ii,:),'edgecolor',c(ii,:));
+    end
 end
 axis tight
 set(gca, 'xtick',0:2:6);
@@ -124,8 +126,8 @@ plot(params(ii,2), rp_scaled(:,ii),'o', 'color', [0 0 0], 'markerfacecolor', c(i
 
 for ii = 2:size(params,1)
     % Plot unscaled as solid line
-    plot([params(ii-1,2) params(ii,2)],[rp(:,ii-1) rp(:,ii)],'-', 'color', c(ii,:));
-    plot([params(ii-1,2) params(ii,2)],[rp_scaled(:,ii-1) rp_scaled(:,ii)],'-', 'color', c(ii,:));
+    plot([params(ii-1,2) params(ii,2)],[rp(:,ii-1) rp(:,ii)],'-', 'color', c(ii,:),'linewidth',3);
+    plot([params(ii-1,2) params(ii,2)],[rp_scaled(:,ii-1) rp_scaled(:,ii)],'-', 'color', c(ii,:),'linewidth',3);
     if any(params(ii,2) == plotparams)
         plot(params(ii,2), rp(:,ii),'o', 'color', c(ii,:), 'markerfacecolor', c(ii,:));
         plot(params(ii,2), rp_scaled(:,ii),'o', 'color', [0 0 0], 'markerfacecolor', c(ii,:));
